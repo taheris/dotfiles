a0044e7f60cd6ed60d00125240f0ca14610c16c3
Shaun Taheri <gpg@taheris.net>
2021-12-05 13:23:06 +0000

    doom: migrate from emacs-config

---
+++ doom/init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+emacs --batch --eval "(progn \
+  (require 'org) \
+  (setq org-confirm-babel-evaluate nil) \
+  (org-babel-tangle-file \"~/.config/doom/config.org\"))"
