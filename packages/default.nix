5d12e2e4e5db286e0b07b9ba9f02a1f346489893
Shaun Taheri <gpg@taheris.net>
2026-01-21 21:34:47 +0000

    Add Apple Pro Display XDR backlight kernel driver package
    Implements USB HID brightness control for Pro Display XDR (05ac:9243)
    and Studio Display (05ac:1114) as an out-of-tree kernel module.
    Based on Julius Zint's apple_bl_usb patch, modified for XDR support.
    
    Creates /sys/class/backlight/apple_xdr_display for DMS integration.

--- packages/default.nix
+++ packages/default.nix
@@ -5,6 +5,9 @@ let
 
 in
 {
+  apple-display-backlight = callPackage ./apple-display-backlight {
+    kernel = pkgs.linuxPackages_latest.kernel;
+  };
   bookerly = callPackage ./bookerly { };
   intercept-fn-keys = callPackage ./intercept-fn-keys { };
   monaco = callPackage ./monaco { };
