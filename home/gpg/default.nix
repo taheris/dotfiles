2b9b31597a6358f0fb8b9239638eea8a8714143c
Shaun Taheri <gpg@taheris.net>
2026-03-13 12:16:12 +0000

    Add gpg keygrip

--- home/gpg/default.nix
+++ home/gpg/default.nix
@@ -11,7 +11,10 @@ let
 in
 {
   home.file = {
-    ".pam-gnupg".text = "9B571D8B78DDF8D06045387D176120709E41CC9D";
+    ".pam-gnupg".text = ''
+      04869B61117CD7602E290767EDF9247026B079CE
+      9B571D8B78DDF8D06045387D176120709E41CC9D
+    '';
   };
 
   home.packages = with pkgs; [
