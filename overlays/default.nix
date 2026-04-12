a3475625b0a8f5a81006beccc1ab6bf2e4c4946a
Shaun Taheri <gpg@taheris.net>
2026-04-12 11:38:29 +0100

    Update flake.lock, overlay ibis-framework fix for duckdb 1.5.1
    Workaround for NixOS/nixpkgs#509064 until it lands in nixos-unstable.

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,6 +14,23 @@
     };
 
   modifications = final: prev: {
+    # TODO: remove once NixOS/nixpkgs#509064 lands in nixos-unstable
+    # Fixes ibis-framework tests against duckdb 1.5.1
+    pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+      (pyFinal: pyPrev: {
+        ibis-framework = pyPrev.ibis-framework.overrideAttrs (old: {
+          pytestFlags = (old.pytestFlags or [ ]) ++ [
+            "-Wignore:fetch_arrow_table:DeprecationWarning"
+            "-Wignore:fetch_record_batch:DeprecationWarning"
+          ];
+          disabledTests = (old.disabledTests or [ ]) ++ [
+            "test_decimal_literal[duckdb-decimal-big]"
+            "test_empty_array_string_join[duckdb]"
+          ];
+        });
+      })
+    ];
+
     typstPackages = prev.typstPackages // {
       moderner-cv = prev.typstPackages.moderner-cv.overrideAttrs (old: {
         version = "0.2.1";
